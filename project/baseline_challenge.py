f35aacff30c1bee3afec2dc41ba4a3d04ad3ff92
Chandan Agrawal <chandan@wadhwaniai.org>
2023-05-12 11:22:25 +0000

    final submission

--- project/baseline_challenge.py
+++ project/baseline_challenge.py
@@ -32,8 +32,8 @@ class BaselineChallenge(FlowSpec):
         
         # load dataset packaged with the flow.
         # this technique is convenient when working with small datasets that need to move to remove tasks.
-        print("tyepe of data", type(data))
-        df = pd.read_csv(self.data)
+        # print("tyepe of data", type(self.data))
+        df = pd.read_csv(io.StringIO(self.data))
         # TODO: load the data. 
         # Look up a few lines to the IncludeFile('data', default='Womens Clothing E-Commerce Reviews.csv'). 
         # You can find documentation on IncludeFile here: https://docs.metaflow.org/scaling/data#data-in-local-files
@@ -56,6 +56,7 @@ class BaselineChallenge(FlowSpec):
 
         self.next(self.baseline, self.model)
 
+    @card
     @step
     def baseline(self):
         "Compute the baseline"
@@ -72,6 +73,7 @@ class BaselineChallenge(FlowSpec):
         self.result = ModelResult("Baseline", params, pathspec, acc, rocauc)
         self.next(self.aggregate)
 
+    @card
     @step
     def model(self):
 
@@ -82,21 +84,75 @@ class BaselineChallenge(FlowSpec):
         # NOTE: If you followed the link above to find a custom model implementation, 
             # you will have noticed your model's vocab_sz hyperparameter.
             # Too big of vocab_sz causes an error. Can you explain why? 
-        self.hyperparam_set = [{'vocab_sz': 100}, {'vocab_sz': 300}, {'vocab_sz': 500}]  
+        self.hyperparam_set = [{'vocab_sz': 100}, {'vocab_sz': 300}, {'vocab_sz': 500}, {'vocab_sz': 600}, {'vocab_sz': 700}]  
         pathspec = f"{current.flow_name}/{current.run_id}/{current.step_name}/{current.task_id}"
 
         self.results = []
         for params in self.hyperparam_set:
             model = NbowModel(**params) # TODO: instantiate your custom model here!
             model.fit(X=self.df['review'], y=self.df['label'])
-            acc = model.eval_acc(self.valdf.review, self.valdf.label) # TODO: evaluate your custom model in an equivalent way to accuracy_score.
-            rocauc = model.eval_rocauc(self.valdf.review, self.valdf.label) # TODO: evaluate your custom model in an equivalent way to roc_auc_score.
+            acc = model.eval_acc(self.valdf.review.values, self.valdf.label) # TODO: evaluate your custom model in an equivalent way to accuracy_score.
+            rocauc = model.eval_rocauc(self.valdf.review.values, self.valdf.label) # TODO: evaluate your custom model in an equivalent way to roc_auc_score.
             self.results.append(ModelResult(f"NbowModel - vocab_sz: {params['vocab_sz']}", params, pathspec, acc, rocauc))
 
         self.next(self.aggregate)
 
+    def add_one(self, rows, result, df):
+        "A helper function to load results."
+        rows.append([
+            Markdown(result.name),
+            Artifact(result.params),
+            Artifact(result.pathspec),
+            Artifact(result.acc),
+            Artifact(result.rocauc)
+        ])
+        df['name'].append(result.name)
+        df['accuracy'].append(result.acc)
+        return rows, df
+
+    @card(type="corise") # TODO: Set your card type to "corise". 
+            # I wonder what other card types there are?
+            # https://docs.metaflow.org/metaflow/visualizing-results
+            # https://github.com/outerbounds/metaflow-card-altair/blob/main/altairflow.py
     @step
     def aggregate(self, inputs):
+
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        from matplotlib import rcParams 
+        rcParams.update({'figure.autolayout': True})
+
+        rows = []
+        violin_plot_df = {'name': [], 'accuracy': []}
+        for task in inputs:
+            if task._name == "model": 
+                for result in task.results:
+                    print(result)
+                    rows, violin_plot_df = self.add_one(rows, result, violin_plot_df)
+            elif task._name == "baseline":
+                print(task.result)
+                rows, violin_plot_df = self.add_one(rows, task.result, violin_plot_df)
+            else:
+                raise ValueError("Unknown task._name type. Cannot parse results.")
+            
+        current.card.append(Markdown("# All models from this flow run"))
+
+        # TODO: Add a Table of the results to your card! 
+        current.card.append(
+            Table(
+                rows, # TODO: What goes here to populate the Table in the card? 
+                headers=["Model name", "Params", "Task pathspec", "Accuracy", "ROCAUC"]
+            )
+        )
+        
+        fig, ax = plt.subplots(1,1)
+        plt.xticks(rotation=40)
+        sns.violinplot(data=violin_plot_df, x="name", y="accuracy", ax=ax)
+        
+        # TODO: Append the matplotlib fig to the card
+        # Docs: https://docs.metaflow.org/metaflow/visualizing-results/easy-custom-reports-with-card-components#showing-plots
+        current.card.append(Image.from_matplotlib(fig))
+
         self.next(self.end)
 
     @step
